55f91005345130b7ca73310306c172a1f1866986
SonTN <sontn@beetsoft.com.vn>
2023-01-04 16:33:11 +0700

    [edit] edit dialog image

--- src/components/DialogImage.js
+++ src/components/DialogImage.js
@@ -1,46 +1,20 @@
-import React from 'react';
-import {Image, StyleSheet, Text, Dimensions, View} from 'react-native';
-import {Modal, ModalContent, ScaleAnimation} from 'react-native-modals';
+import React from "react";
+import { Image, StyleSheet, Text, Dimensions, View } from "react-native";
+import { Modal, ModalContent, ScaleAnimation } from "react-native-modals";
 
 function areEqual(prevProps, nextProps) {
   return prevProps.isModalVisible === nextProps.isModalVisible;
 }
 
 function getWindownWidth() {
-  return Math.round(Dimensions.get('window').width * 1000) / 1000 - 6;
+  return Math.round(Dimensions.get("window").width * 1000) / 1000 - 6;
 }
 
+let screenWidth = Dimensions.get("window").width;
+let screenHeight = Dimensions.get("window").height;
+
 function DialogImage(props) {
-  const {isModalVisible, imageUrl, indexImage} = props;
-
-  function getWidthImage(index) {
-    const columnWidth = getWindownWidth() / 3;
-    var imageWidth = 0;
-    switch (index) {
-      case index % 3 === 0:
-        imageWidth = columnWidth * 3;
-        break;
-      case index % 2 === 0:
-        imageWidth = columnWidth * 2;
-        break;
-      default:
-        return (imageWidth = columnWidth);
-    }
-  }
-
-  function getHeightImage(index) {
-    var imageHeight = 0;
-    switch (index) {
-      case index % 3 === 0:
-        imageHeight = 300;
-        break;
-      case index % 2 === 0:
-        imageHeight = 250;
-        break;
-      default:
-        return (imageHeight = 250);
-    }
-  }
+  const { isModalVisible, imageUrl, indexImage } = props;
 
   React.useLayoutEffect(() => {}, []);
 
@@ -53,19 +27,12 @@ function DialogImage(props) {
             initialValue: 0, // optional
             useNativeDriver: true, // optional
           })
-        }>
-        <ModalContent
-          style={{
-            width: getWidthImage(indexImage),
-            height: getHeightImage(indexImage),
-            flex: 1,
-            backgroundColor: 'yellow',
-          }}>
+        }
+      >
+        <ModalContent style={styles.modalCenter}>
           <View>
-            <Image style={styles.image} source={{uri: imageUrl}} />
+            <Image style={styles.image} source={{ uri: imageUrl }} />
             <Text> `index: ${indexImage}`</Text>
-            <Text> `width: ${getWidthImage(indexImage)}`</Text>
-            <Text> `height: ${getHeightImage(indexImage)}`</Text>
           </View>
         </ModalContent>
       </Modal>
@@ -77,13 +44,16 @@ const styles = StyleSheet.create({
   modalContainer: {},
   modalCenter: {
     flex: 1,
-    backgroundColor: 'yellow',
+    width: screenWidth - 40,
+    height: screenHeight * 0.5,
+    maxHeight: screenHeight * 0.6,
+    backgroundColor: "yellow",
   },
   image: {
-    // width: '100%',
-    // height: '100%',
+    width: "100%",
+    height: "100%",
     // resizeMode: 'center',
-    backgroundColor: 'red',
+    backgroundColor: "red",
   },
 });
 export default React.memo(DialogImage, areEqual);
